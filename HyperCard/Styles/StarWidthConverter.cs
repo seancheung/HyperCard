8af80615b9d28718e2476545bb4199be616e0b79
Sean <theoxuanx@gmail.com>
2014-04-05 13:31:45 +0800

    GridViewColumn Width Auto Adjusment

---
+++ HyperCard/Styles/StarWidthConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace HyperCard.Styles
+{
+    public class StarWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ListView listview = value as ListView;
+            double width = listview.ActualWidth;
+            GridView gv = listview.View as GridView;
+            for (int i = 0; i < gv.Columns.Count; i++)
+            {
+                if (!Double.IsNaN(gv.Columns[i].Width))
+                    width -= gv.Columns[i].ActualWidth;
+            }
+            return width - 5;// this is to take care of margin/padding
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
